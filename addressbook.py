e6d13abe5645b2d4966c287b468929a138df7f69
joczarnocka <42293202+joczarnocka@users.noreply.github.com>
2022-08-06 16:41:08 +0200

    add function create_contacts

--- addressbook.py
+++ addressbook.py
@@ -30,6 +30,9 @@ class BusinessContact(BaseContact):
         self.company = company
         self.busines_phone = business_phone
 
+    def __str__(self) -> str:
+         return f"{self.firstname} {self.surname}: {self.email} ( {self.role}, {self.company})"
+
     def contact(self) -> None:
         print(f"Wybieram numer {self.busines_phone} i dzwonię do {self.firstname} {self.surname}.")
 
@@ -42,9 +45,26 @@ def giveCard() -> BaseContact:
     return BaseContact(firstname, surname, phone, email)
 
 
+def create_contacts(card_type, card_number) -> list:
+    res_list = []
+    for i in range(card_number):
+        firstname = fake.first_name()
+        surname = fake.last_name()
+        phone = fake.phone_number()
+        email= f"{firstname}.{surname}@{fake.domain_name()}"
+        if card_type == "base":
+            card = BaseContact(firstname, surname, phone, email)
+        else:
+            role = fake.job()
+            phone2 = fake.phone_number()
+            company = fake.company()
+            card = BusinessContact(role,company,phone2,firstname, surname,phone, email)
+        res_list.append(card) 
+    return res_list
 
 if __name__ == "__main__":
-    pass
+    # bc = BusinessContact("engineer","IBM", "+48 030349534","John","Smith","+48 0324934290", "john.smith@ibm.com")
+    # print(bc.name_length)
     # card_list = [ContactCard("Adam","Adamski","CompanyA","adam@gmail.com"),
     #              ContactCard("Bartek","Bartkowski","CompanyB","bartek@gmail.com"),
     #              ContactCard("Cezary","Cezarski","CompanyC","cezary@gmail.com"),
@@ -67,3 +87,8 @@ if __name__ == "__main__":
     #     print(card)
 
     # print(by_email[0].name_length)  
+
+    cards_list = create_contacts("business", 4)
+
+    for card in cards_list:
+        print(card)
